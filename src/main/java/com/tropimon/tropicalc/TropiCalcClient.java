e9ead807f554dfca6997f1f00af5168f6e3ffa43
mrsilverfog-hash <mrsilverfog@gmail.com>
2026-06-30 23:55:10 +0200

    Update TropiCalcClient.java

--- src/main/java/com/tropimon/tropicalc/TropiCalcClient.java
+++ src/main/java/com/tropimon/tropicalc/TropiCalcClient.java
@@ -1,22 +1,19 @@
 package com.tropimon.tropicalc;
 
+import com.tropimon.tropicalc.calc.SmogonDataLoader;
 import com.tropimon.tropicalc.client.CalcOverlay;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Point d'entrée client de TropiCalc.
- * Calculateur de dégâts automatique pour les combats Cobblemon en simple
- * (1 contre 1), avec estimation des sets adverses en temps réel.
- */
 public class TropiCalcClient implements ClientModInitializer {
     public static final String MOD_ID = "tropicalc";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {
+        SmogonDataLoader.charger();
         HudRenderCallback.EVENT.register(new CalcOverlay());
         LOGGER.info("TropiCalc chargé - moteur de calcul de dégâts (format Simple uniquement)");
     }
